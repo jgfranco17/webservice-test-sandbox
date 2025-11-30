ab2a0de3001287fe38161abeaa3b436bd05c3e19
Chino Franco <chino.franco@gmail.com>
2025-11-30 16:04:54 +0900

    fix: use const for env variable

--- tests/testutils/conditions.py
+++ tests/testutils/conditions.py
@@ -1,10 +1,13 @@
 import os
+from typing import Final
 
 import pytest
+
+ENV_INTEGRATION_TEST: Final[str] = "RUN_INTEGRATION"
 
 
 def integration_test(testcase_id: str) -> pytest.MarkDecorator:
     """Mark a test as an integration test."""
-    is_integration = os.getenv("RUN_INTEGRATION") is None
-    reason = f"[{testcase_id}] Needs live instance of server"
+    is_integration = os.getenv(ENV_INTEGRATION_TEST) is None
+    reason = f"[{testcase_id}] Skipping as this test requires a live instance of server"
     return pytest.mark.skipif(condition=is_integration, reason=reason)
